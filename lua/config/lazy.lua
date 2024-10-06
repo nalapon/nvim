cc03aebe1119cce73b71f2149425574d6e9d7b49
Náder Aladel Ponce <naladelponce@kolokium.com>
2024-10-06 12:01:50 +0200

    lsp working, copilot not working
    Trying to add LSP config and copilot config and autocomplete an
    suggestion. Not working as intended for now

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -73,7 +73,13 @@ vim.opt.scrolloff = 10
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.diagnostic.config {
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = true,
+  severity_sort = false,
+}
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- lua/plugins/harpoon2.lua
+++ lua/plugins/harpoon2.lua
@@ -1,22 +1,22 @@
 return {
-  "ThePrimeagen/harpoon",
-  branch = "harpoon2",
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
   config = function()
-    local harpoon = require "harpoon"
+    local harpoon = require 'harpoon'
     harpoon:setup()
 
-    vim.keymap.set("n", "<leader>a", function()
+    vim.keymap.set('n', '<leader>a', function()
       harpoon:list():add()
-    end, { desc = "Add to harpoon" })
-    vim.keymap.set("n", "<C-e>", function()
+    end, { desc = 'Add to harpoon' })
+    vim.keymap.set('n', '<C-e>', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end)
 
     -- Set <space>1..<space>5 be my shortcuts to moving to the files
     for _, idx in ipairs { 1, 2, 3, 4, 5 } do
-      vim.keymap.set("n", string.format("<space>%d", idx), function()
+      vim.keymap.set('n', string.format('<space>%d', idx), function()
         harpoon:list():select(idx)
-      end)
+      end, { desc = 'Select between harpooned files' })
     end
   end,
 }
